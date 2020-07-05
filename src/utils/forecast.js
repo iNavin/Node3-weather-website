8e0a05501801f2ccfd58b54af70d51a249d1392c
Navin Stark <i.navinkumar1998@gmail.com>
2020-07-05 18:20:49 +0530

    changes made to the about page and forecast method

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) =>{
        }else if(body.error){
           callback('Unable to get data for the specified location',undefined);
        }else{
-          callback(undefined,body.current.weather_descriptions+" .The current temperature is "+body.current.temperature+" But feels like "+body.current.feelslike)
+          callback(undefined,body.current.weather_descriptions+" .The current temperature is "+body.current.temperature+" But feels like "+body.current.feelslike+"\n the humidity : "+body.current.humidity)
        }
     })
  
