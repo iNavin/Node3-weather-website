6c24219ff2c12e48a030e43b46cf395d0aa7c0c0
Navin Stark <i.navinkumar1998@gmail.com>
2020-07-05 11:43:37 +0530

    initial commit learning

---
+++ public/js/app.js
@@ -0,0 +1,43 @@
+//const { response } = require("express");
+
+console.log('client side javascript file');
+
+// fetch('http://localhost:3000/weather?address=Boston').then((response)=>{
+//     response.json().then((data) => {
+//         if(data.error){
+//             console.log(data.error);
+//         }else{
+//             console.log(data);
+//         }
+//     })
+// })
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const message1 = document.querySelector('#message-1');
+const message2 =document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+    const url = 'http://localhost:3000/weather?address='+location;
+
+    message1.textContent = 'Loading...';
+    message2.textContent = '';
+
+    fetch(url).then((response)=>{
+    response.json().then((data) => {
+        if(data.error){
+            message1.textContent= data.error
+            
+        }else{
+            message1.textContent ='Forecast : '+ data.data;
+            message2.textContent ='Location : '+data.location ;
+            console.log(data)
+        }
+    })
+})
+
+    
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) =>{
+    const url = 'http://api.weatherstack.com/current?access_key=5768ab041e20f245a9a39498ea9e2c02&query='+latitude+','+longitude;
+ 
+    request({url, json:true}, (error, {body}) => {
+       if(error){
+          callback('Unable to connect to the web server',undefined);
+       }else if(body.error){
+          callback('Unable to get data for the specified location',undefined);
+       }else{
+          callback(undefined,body.current.weather_descriptions+" .The current temperature is "+body.current.temperature+" But feels like "+body.current.feelslike)
+       }
+    })
+ 
+ }
+
+ module.exports = forecast;
