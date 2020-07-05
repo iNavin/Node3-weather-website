30fcd32836f9e49b9da655b9d4022744310632a0
Navin Stark <i.navinkumar1998@gmail.com>
2020-07-05 17:46:38 +0530

    weather lines test

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,3 @@
-//const { response } = require("express");
-
-//console.log('client side javascript file');
-
-// fetch('http://localhost:3000/weather?address=Boston').then((response)=>{
-//     response.json().then((data) => {
-//         if(data.error){
-//             console.log(data.error);
-//         }else{
-//             console.log(data);
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const message1 = document.querySelector('#message-1');
@@ -41,3 +27,5 @@ weatherForm.addEventListener('submit', (e) => {
 
     
 })
+
+console.log('test')
